dff855be92f082116176b43baa3ce58633bde37c
Xzenoe <bogdantepajkin@gmail.com>
2024-12-07 15:45:59 +0200

    a

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,8 @@
 'use strict';
-
-const pipe = (...fns) => (x) => null;
-
+const pipe = (...fns) => {
+  for (const i of fns) {
+    if (typeof i !== 'function') throw 'all elements should be functions';
+  }
+  return (x) => fns.reduce((acc, curr) => acc = curr(acc), x);
+};
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,26 @@
 'use strict';
 
-const compose = (...fns) => (x) => null;
-
+const compose = (...fns) => {
+  const err = [];
+  const compose = (x) => {
+    let last = fns.length - 1;
+    let result = x;
+    try {
+      for (last; last >= 0; last--) {
+        result = fns[last](result);
+      }
+      return result;
+    } catch (error) {
+      for (const er of err) {
+        er(error);
+      }
+      return null;
+    }
+  };
+  compose.on = (y, x) => {
+    if (y === 'error') err.push(x);
+  };
+  return compose;
+};
+// Навіть код в солюшенах не може пройти тести, я взагалі заплутався що робити з цими ерорами, вибачте що код в цьому завданні дуже схожий на солюшен
 module.exports = { compose };
